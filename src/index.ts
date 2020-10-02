38757059128684fbfe34f0ac21fcf4f0ccb877f5
Sangwon <jackoss@naver.com>
2020-10-03 03:19:15 +0900

    0.8 Blockchain Creating a Block

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,28 @@
-class Human {
-    public name: string;
-    public age: number;
-    public gender: string;
-    constructor(name:string, age:number, gender:string){
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+class Block {
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+    constructor(
+        index: number,
+        hash: string,
+        previousHash: string,
+        data: string,
+        timestamp: number,
+    ){
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
     }
 }
 
-const sangwon = new Human("sangwon", 23, "male");
+const genesisBlock:Block = new Block(0, "20202020", "", "Hello", 123456);
 
-// interface Human {
-//     name: string;
-//     age: number;
-//     gender: string;
-// }
+let blockchain:[Block] = [genesisBlock];
 
-// const person = {
-//     name: "sangwon",
-//     gender: "male",
-//     age: 24
-// }
+console.log(blockchain);
 
-const sayHi = (person: Human):string => {// ?가 붙으면 optional
-    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
-}
-
-console.log(sayHi(sangwon));
-
-export {}//버그 해결을 위해 넣어줌
+export {}
