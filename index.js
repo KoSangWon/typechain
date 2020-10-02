ae8205b6ed20ab5abf06cf46621cd404c49a6e1d
Sangwon <jackoss@naver.com>
2020-10-03 01:50:54 +0900

    #0.4 First steps with Typescript

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-console.log('hi');
+"use strict";
+// Node.js는 TypeScript를 이해하지 못하기 때문에 일반적인 JavaScript 코드로 컴파일하는 작업이 필요! pacakge.json에서 prestart로 tsc 컴파일과정 설정해줌.
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Sangwon", age = 24, gender = "male";
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+sayHi(name, age);
 //# sourceMappingURL=index.js.map
